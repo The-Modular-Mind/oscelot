e096a1bc7b62ccf184de730871234c8dc2b35442
theModularMind <you@example.com>
2021-08-20 01:18:02 -0700

    Removing unused code

--- src/components.hpp
+++ src/components.hpp
@@ -2,23 +2,3 @@
 #include "plugin.hpp"
 #include "components/PawButtons.hpp"
 #include "ui/ThemedModuleWidget.hpp"
-
-struct TriggerParamQuantity : ParamQuantity {
-	std::string getDisplayValueString() override {
-		return ParamQuantity::getLabel();
-	}
-	std::string getLabel() override {
-		return "";
-	}
-};
-
-struct BufferedTriggerParamQuantity : TriggerParamQuantity {
-	float buffer = false;
-	void setValue(float value) override {
-		if (value >= 1.f) buffer = true;
-		TriggerParamQuantity::setValue(value);
-	}
-	void resetBuffer() {
-		buffer = false;
-	}
-};
